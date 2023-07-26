33ed4dcc7495f7e78802027c9726de1bf3261689
Carlos Eduardo C. do Val <kadu20es@gmail.com>
2023-07-25 23:09:02 -0300

    Iniciada criação do cálculo de tempo

--- app.js
+++ app.js
@@ -1,33 +1,64 @@
 console.log("teste");
 
-const day = document.querySelector('#day');
-const month = document.querySelector('#month');
-const year = document.querySelector('#year');
+const DAY = document.querySelector('#day');
+const MONTH = document.querySelector('#month');
+const YEAR = document.querySelector('#year');
 
-const days = document.querySelector('#days');
-const months = document.querySelector('#months');
-const years = document.querySelector('#years');
+const DAYS = document.querySelector('#days');
+const MONTHS = document.querySelector('#months');
+const YEARS = document.querySelector('#years');
 
-const now = new Date;
+let validDay, validMonth, validYear;
 
+// dd/MM/yyyy
+//const now = new Date().toLocaleString().substring(0, 10);
+const now = new Date();
 
-day.addEventListener('keyup', () => {
-    let value = day.value;
-    if (value < 1 || value > 31){
+let birthDate = new Date();
+
+DAY.addEventListener('keyup', () => {
+    validDay = DAY.value;
+    if (validDay < 1 || validDay > 31){
         console.log("Insira um dia válido");
     }
+    birthDate.setDate(validDay);
 });
 
-month.addEventListener('keyup', () => {
-    let value = month.value;
-    if (value < 1 || value > 12) {
+MONTH.addEventListener('keyup', () => {
+    validMonth = MONTH.value;
+    if (validMonth < 1 || validMonth > 12) {
         console.log("insira um mês válido");
     }
+    if (validDay > 28 && validMonth == 2){
+        console.log("fevereiro só tem 28 dias");
+    }
+    birthDate.setMonth(validMonth-1);
 });
 
-year.addEventListener('keyup', () => {
-    let value = year.value;
-    if (value < now.getFullYear() - 120 || value > now.getFullYear()){
+YEAR.addEventListener('keyup', () => {
+    validYear = YEAR.value;
+    if (validYear < now.getFullYear() - 120 || validYear > now.getFullYear()){
         console.log("Insira um ano válido");
     }
+
+    if (validYear % 4 == 0){
+        console.log("ano bissexto");
+    }
+    birthDate.setFullYear(validYear);
+    calculate(birthDate);
 });
+
+function calculate(birthDate){
+    console.log(birthDate);
+
+
+    let novaData = new Date(now - birthDate);
+    console.log(novaData)
+    let novaData2 = (novaData.toISOString().slice(0, 4) - 1970) + "Y " + novaData.getMonth() + "M " + (novaData.getDate()-1 + "D ");
+    console.log(novaData2);
+
+    let message = `Você nasceu a ${years} anos, ${months} meses e ${days} dias!`;
+    console.log(message);
+}
+
+calculate();
